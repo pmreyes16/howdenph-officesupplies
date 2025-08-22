1db4532a6681ff7e9bfd4b275f26c8ea07443d39
pmreyes16 <pmreyes@ph.howdengroup.com>
2025-08-22 15:11:48 +0800

    run build

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,29 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // All 3rd party libraries go into vendor chunk
+          }
+
+          if (id.includes("components")) {
+            return "components"; // All components go into components chunk
+          }
+
+          return "index"; // Default chunk
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
   },
-  plugins: [
-    react()
-  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
